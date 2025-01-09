cb67efb6db1f7a9a93be255102a3698911536b66
DiegoM <diego.munoz@eurekatech.io>
2025-01-09 17:02:37 -0300

    fix title, create super user auto

--- build.sh
+++ build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+python manage.py shell <<EOF
+from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'password123')
+EOF
