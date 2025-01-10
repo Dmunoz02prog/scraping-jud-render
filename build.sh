ac9f8d1311dad2da745ef32f35bd20b10bb7c74e
DiegoM <diego.munoz@eurekatech.io>
2025-01-09 21:15:48 -0300

    build

--- build.sh
+++ build.sh
@@ -7,16 +7,15 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
 
+python manage.py runserver 0.0.0.0:$PORT
+
 python manage.py shell <<EOF
 from django.contrib.auth.models import User
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@example.com', 'password123')
 EOF
 
-gunicorn --bind 0.0.0.0 datascrap.wsgi:application &
-
 echo "Iniciando Celery beat..."
 nohup celery -A datascrap worker --pool=solo --loglevel=info & 
 
 nohup celery -A datascrap beat --loglevel=info &
-
