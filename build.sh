7c279255fc414b6ccd8d5e2eb32679869f77db2c
DiegoM <diego.munoz@eurekatech.io>
2025-01-09 22:17:45 -0300

    build

--- build.sh
+++ build.sh
@@ -1,24 +1,38 @@
 #!/usr/bin/env bash
+
 echo "INICIO###########..."
 set -o errexit
 
+# Instalación de dependencias (si es necesario)
 # pip install -r requirements.txt
 
+# Recolección de archivos estáticos
+echo "Recolectando archivos estáticos..."
 python manage.py collectstatic --noinput
+echo "Archivos estáticos recolectados."
+
+# Migración de base de datos
+echo "Ejecutando migraciones..."
 python manage.py migrate
+echo "Migraciones ejecutadas."
 
-# python manage.py runserver 0.0.0.0:$PORT
-
-python manage.py shell <<EOF
-from django.contrib.auth.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'password123')
-EOF
-
+# Verifica la instalación de chromedriver
 echo "Verificando instalaciones existentes de Google Chrome y chromedriver..."
-chromedriver --version  echo "Chromedriver no está instalado"
+if chromedriver --version; then
+    echo "Chromedriver está instalado."
+else
+    echo "Chromedriver no está instalado. Verifica tu entorno."
+fi
 
-echo "Iniciando Celery beat..."
-nohup celery -A datascrap worker --pool=solo --loglevel=info & 
+# Inicia Celery Worker
+echo "Iniciando Celery Worker..."
+nohup celery -A datascrap worker --pool=solo --loglevel=info > celery_worker.log 2>&1 &
+echo "Celery Worker iniciado."
 
-nohup celery -A datascrap beat --loglevel=info &
+# Inicia Celery Beat
+echo "Iniciando Celery Beat..."
+nohup celery -A datascrap beat --loglevel=info > celery_beat.log 2>&1 &
+echo "Celery Beat iniciado."
+
+# Mantiene el contenedor activo
+echo "Manteniendo el contenedor activo..."
